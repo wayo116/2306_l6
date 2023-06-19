ff5d8f8d0d0e45fb907097746db1fd46205c4a26
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-06-19 21:58:51 +0900

    Update Dell6_v2.py

--- Dell6_v2.py
+++ Dell6_v2.py
@@ -139,7 +139,7 @@ class Dell6:
         print('予想個数{}'.format(len(dlist1)))
         print('予想当選額{0}'.format(-len(dlist1)*0.02+cnt3*0.1+cnt4*0.9+cnt5*30+cnt6*10000))
 
-    def merge_similar_elements(matrix, similarity_threshold):
+    def merge_similar_elements(self, matrix, similarity_threshold):
         merged_matrix = []
         for row in matrix:
             merged_row = []
