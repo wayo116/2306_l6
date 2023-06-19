ce179bba9e0b8948c91edeca46e1c2216b230c0d
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-06-19 12:14:30 +0900

    Delete combi.py

--- combi.py
+++
@@ -1,9 +0,0 @@
-import itertools
-
-def combi(num_list,combisu):
-    print('----combi----')
-    new_list = []
-    for num in itertools.combinations(num_list, combisu):
-        new_list.append(list(num))
-    #print('new_list',new_list)
-    return new_list
