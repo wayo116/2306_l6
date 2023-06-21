bfb43839c9c4f0f2c29179ae7650d475e43d4fe6
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-06-21 14:21:09 +0900

    Add files via upload

--- Utility/kfind.py
+++ Utility/kfind.py
@@ -1,52 +1,70 @@
 from datalists import dlists
 from collections import Counter
 
-st = 0
-ed = 100
-taisho_hani = 50
-fbox_summary = []
-for cnt in range(taisho_hani):
-    print("st:"f"{st}" "-" "ed:"f"{ed}")
-    new_dlists = dlists[st:ed]
-    # print(new_dlists)
+def kfind(dlist, taisho_hani):
+    st = 0
+    ed = 100
+    # taisho_hani = 50
+    fbox_summary = []
+    for cnt in range(taisho_hani):
+        # print("st:"f"{st}" "-" "ed:"f"{ed}")
+        new_dlists = dlists[st:ed]
+        # print(new_dlists)
 
-    print("new_dlists[0]:" f"{new_dlists[0]}")
+        # print("new_dlists[0]:" f"{new_dlists[0]}")
 
-    fbox = []
-    for dyouso in new_dlists[0]:
-        print("dyouso:" f"{dyouso}")
+        fbox = []
+        for dyouso in new_dlists[0]:
+            # print("dyouso:" f"{dyouso}")
 
-        for ii in range(1,len(new_dlists)):
-            print(new_dlists[ii])
-            if dyouso in new_dlists[ii]:
-                print("hit-idx:" f"{ii}\n")
-                fbox.append(ii)
-                break
-    print("fbox:"f"{fbox}\n")
+            for ii in range(1,len(new_dlists)):
+                # print(new_dlists[ii])
 
-    fbox_summary.append(fbox)
+                if dyouso in new_dlists[ii]:
+                    # print("hit-idx:" f"{ii}\n")
+                    fbox.append(ii)
+                    break
 
-    st = st + 1
-    ed = ed + 1
+        # print("fbox:"f"{fbox}\n")
 
-print(fbox_summary)
+        fbox_summary.append(fbox)
 
-data = fbox_summary
-# データをフラットなリストに変換
-flat_data = [item for sublist in data for item in sublist]
+        st = st + 1
+        ed = ed + 1
 
-# 数字の出現回数をカウント
-counts = Counter(flat_data)
+    # print(fbox_summary)
 
-# 出現回数順にソート
-sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
 
-# よく出現する数字の範囲を取得
-frequent_numbers = [item[0] for item in sorted_counts[:10]]  # 出現回数の上位3つを取得
+    data = fbox_summary
+    # データをフラットなリストに変換
+    flat_data = [item for sublist in data for item in sublist]
 
-# 外れ値の範囲を取得
-outlier_numbers = [item[0] for item in sorted_counts[-10:]]  # 出現回数の下位3つを取得
+    # 数字の出現回数をカウント
+    counts = Counter(flat_data)
 
-# 結果の表示
-print("よく出現する数字の範囲:", min(frequent_numbers), "-", max(frequent_numbers))
-print("外れ値の範囲:", min(outlier_numbers), "-", max(outlier_numbers))
+    # 出現回数順にソート
+    sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+
+    # よく出現する数字の範囲を取得
+    frequent_numbers = [item[0] for item in sorted_counts[:10]]  # 出現回数の上位*つを取得
+
+    # 外れ値の範囲を取得
+    outlier_numbers = [item[0] for item in sorted_counts[-10:]]  # 出現回数の下位*つを取得
+
+    # 結果の表示
+    # print("よく出現する数字:", sorted_counts)
+    # print("よく出現する数字の範囲:", min(frequent_numbers), "-", max(frequent_numbers))
+    # print("外れ値の範囲:", min(outlier_numbers), "-", max(outlier_numbers))
+
+    inner_list = []
+    outer_list = []
+    inner_list.append(min(frequent_numbers))
+    inner_list.append(max(frequent_numbers))
+    outer_list.append(min(outlier_numbers))
+    outer_list.append(max(outlier_numbers))
+    print("inner_list:"f"{inner_list}\n" "outer_list:"f"{outer_list}\n")
+
+    return inner_list, outer_list
+
+# inner_list, outer_list = kfind(dlists,20)
+
