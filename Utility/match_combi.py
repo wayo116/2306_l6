80106a1a63e1c40364b979e7292d4d621220b649
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-06-28 09:55:31 +0900

    Update match_combi.py

--- Utility/match_combi.py
+++ Utility/match_combi.py
@@ -1,11 +1,36 @@
 #from datalists import dlists
 import numpy as np
-from Utility.matching import yobu
+from Utility.matching import yobu, aisho
 import itertools
 
 
 def yobu_combi(dlists, kaisus, sikichi):
     dcnt = yobu(dlists, kaisus)
+
+    result = []
+    for youso in dlists[0]:
+        result.append([idx+1 for idx, item in enumerate(dcnt[youso-1]) if item >= sikichi])
+
+    print(result)
+
+    newlists=[]
+    for no1 in result[0]:
+        for no2 in result[1]:
+            for no3 in result[2]:
+                for no4 in result[3]:
+                    for no5 in result[4]:
+                        for no6 in result[5]:
+                            newlist=[]
+                            newlist = sorted([no1]+[no2]+[no3]+[no4]+[no5]+[no6])
+                            if len(set(newlist)) == 6:
+                                # print(newlist)
+                                newlists.append(newlist)
+    print('match_combi組合せ数',len(newlists))
+    return newlists
+
+
+def aisho_combi(dlists, kaisus, sikichi):
+    dcnt = aisho(dlists, kaisus)
 
     result = []
     for youso in dlists[0]:
