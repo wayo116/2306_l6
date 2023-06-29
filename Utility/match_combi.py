664433b19a49d5015be0249902451200f85dfc89
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-06-29 09:40:28 +0900

    Add files via upload

--- Utility/match_combi.py
+++ Utility/match_combi.py
@@ -53,6 +53,77 @@ def aisho_combi(dlists, kaisus, sikichi):
     print('match_combi組合せ数',len(newlists))
     return newlists
 
+
+def yobu_lists(dlists, kaisus, sikichi):
+    dcnt = yobu(dlists, kaisus)
+
+    result = []
+    for youso in dlists[0]:
+        result.append([idx+1 for idx, item in enumerate(dcnt[youso-1]) if item >= sikichi])
+
+    print(result)
+
+    return result
+
+
+def aisho_lists(dlists, kaisus, sikichi):
+    dcnt = aisho(dlists, kaisus)
+
+    result = []
+    for youso in dlists[0]:
+        result.append([idx+1 for idx, item in enumerate(dcnt[youso-1]) if item >= sikichi])
+
+    print(result)
+
+    return result
+
+
+def yobu_aisho_combi(dlists, yobu_lists, aisho_dcnt, sikichi):
+
+    result = []
+    for ii in range(6):
+        temp = []
+        print("yobu_lists[" f"{ii}" "]=" f"{yobu_lists[ii]}")
+
+        for youso in yobu_lists[ii]:
+            print("youso",youso)
+            print("aisho_dcnt[" f"{youso-1}" "]=" f"{aisho_dcnt[youso-1]}")
+            temp.extend([idx+1 for idx, item in enumerate(aisho_dcnt[youso-1]) if item >= sikichi])
+        print("temp",temp)
+
+        sorted_result = sorted(set(temp)) 
+        print("sorted_result",sorted_result)
+
+        result.append(sorted_result)
+
+    print("result",result)
+
+    newlists=[]
+    for no1 in result[0]:
+        for no2 in result[1]:
+            for no3 in result[2]:
+                for no4 in result[3]:
+                    for no5 in result[4]:
+                        for no6 in result[5]:
+                            newlist=[]
+                            newlist = sorted([no1]+[no2]+[no3]+[no4]+[no5]+[no6])
+                            if len(set(newlist)) == 6:
+                                # print(newlist)
+                                newlists.append(newlist)
+    print('match_combi組合せ数',len(newlists))
+    return newlists
+
 # kaisus = 1500
 # sikichi = 10
 # yobu_combi(dlists, kaisus, sikichi)
+
+# from datalists import dlists
+# kaisus = 50
+# sikichi = 1
+# yobu_lists = yobu_lists(dlists, kaisus, sikichi)
+
+# kaisus = 50
+# aisho_dcnt = aisho(dlists, kaisus)
+
+# sikichi = 4
+# yobu_aisho_combi(dlists, yobu_lists, aisho_dcnt, sikichi)
