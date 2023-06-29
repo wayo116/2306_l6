1ce8f6f4ad9ba1592a5146d82e92cd253038d033
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-06-30 00:04:34 +0900

    Update max_in_min.py

--- Utility/max_in_min.py
+++ Utility/max_in_min.py
@@ -2,5 +2,8 @@ def max_in_min(dcnts):
     result=[]
     for dcnt in dcnts:
         result.append(max(dcnt))
+        
+    print("result",result)
     print("閾値",min(set(result))) 
+    
     return min(set(result))
