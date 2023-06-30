cb7ce2cdb0a2a7a64089f17350d81d6ce8ea5372
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-06-30 22:03:51 +0900

    Update tate_hani.py

--- Utility/tate_hani.py
+++ Utility/tate_hani.py
@@ -1,2 +1,9 @@
-def tate_hani(dlists, kaisu):
-    for dlist in dlists:
+def tate_hani(dlists, kaisus):
+    results=[]
+    for ii in range(6):
+        result=[]
+        for kaisu in kaisus:
+            result.append(dlists[kaisu][ii])
+        results.append(result)
+
+    print(results)
