0991a22cfbfe6430096d96f1ea385278cf6a13b8
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-07-01 23:47:19 +0900

    Update tate_hani.py

--- Utility/tate_hani.py
+++ Utility/tate_hani.py
@@ -1,8 +1,25 @@
+
+import collections
+
 def tate_hani(dlists, kaisus):
     results=[]
     for ii in range(6):
         result=[]
         result = [dlists[kaisu][ii] for kaisu in range(kaisus)]
+        results.append(result)
+
+    print(results)
+    
+    return results
+
+def tate_hani2(dlists, kaisus, topx):
+    results=[]
+    for ii in range(6):
+        result=[]
+        result = [dlists[kaisu][ii] for kaisu in range(kaisus)]
+        counts = zip(*c.most_common())
+        counts = counts[topx]
+        result = [min(counts),max(counts)]
         results.append(result)
 
     print(results)
