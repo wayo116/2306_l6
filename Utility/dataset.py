fa5d507dd01e211dbfdf248ee7c0fecb0299213b
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-07-11 09:31:20 +0900

    Update dataset.py

--- Utility/dataset.py
+++ Utility/dataset.py
@@ -1,36 +1,21 @@
 from datalists import dlists 
 import random
 
+import numpy as np
+from collections import Counter
 
-# def no_dataset(dlists, no, target_kaisu_list):
-#     no_dataset = []
-#     for kaisu, dlist in enumerate(dlists):
-
-#         kaisu_limit = len(dlists)-max(target_kaisu_list)
-#         # print("kaisu_limit",kaisu_limit)
-#         if kaisu >= kaisu_limit:
-#             break
-
-#         tmp = []
-#         for dlist_idx, dlist_youso in enumerate(dlist):
-#             if dlist_youso == no:
-#                 tmp.append(no)
-#                 for target_kaisu_youso in target_kaisu_list:
-#                     tmp.append(dlists[kaisu+target_kaisu_youso][dlist_idx])
-
-#         if tmp != []:
-#             no_dataset.append(tmp)
-         
-#     print("no_dataset",no_dataset)
-#     print("no_dataset_len",len(no_dataset))
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+# import pandas as pd
+import lightgbm as lgb
 
 
 def no_dataset(dlists, target_kaisu_lists):
 
     no_dataset = []
-    for dlist_idx in range(6):
+    for dlist_retu in range(6):
 
-        target_kaisu_list = target_kaisu_lists[dlist_idx]
+        target_kaisu_list = target_kaisu_lists[dlist_retu]
         
         for kaisu, dlist in enumerate(dlists):
 
@@ -40,14 +25,25 @@ def no_dataset(dlists, target_kaisu_lists):
                 break
 
             tmp = []
-            tmp.append(dlist[dlist_idx])
+            tmp.append(dlist[dlist_retu])
             for target_kaisu_youso in target_kaisu_list:
-                tmp.append(dlists[kaisu+target_kaisu_youso][dlist_idx])
+                tmp.append(dlists[kaisu+target_kaisu_youso][dlist_retu])
 
             if tmp != []:
-                no_dataset.append(tmp)
+                # no_dataset.append(tmp)
+
+                list1 = tmp[1:]
+                # print("list1",list1)
+                list2s = create_random_lists_float(range_start=0.1*-1, range_end=0.1, yousosu=len(target_kaisu_list), listsu=100)
+                for list2 in list2s:
+                    # print("list2",list2)
+                    result = [x + y for x, y in zip(list1, list2)]
+                    # print("result",result)
+                    result.insert(0, dlist[dlist_retu])
+                    # print("result_in",result)
+                    no_dataset.append(result)
          
-    print("no_dataset",no_dataset)
+    # print("no_dataset",no_dataset)
     print("no_dataset_len",len(no_dataset))
 
     return no_dataset
@@ -56,24 +52,37 @@ def no_dataset(dlists, target_kaisu_lists):
 def no_dataset_test(dlists, target_kaisu_lists):
 
     no_dataset = []
-    for dlist_idx in range(6):
+    for dlist_retu in range(6):
 
-        target_kaisu_list = target_kaisu_lists[dlist_idx]
+        target_kaisu_list = target_kaisu_lists[dlist_retu]
         
         tmp = []
         for target_kaisu_youso in target_kaisu_list:
-            tmp.append(dlists[target_kaisu_youso][dlist_idx])
+            tmp.append(dlists[target_kaisu_youso][dlist_retu])
 
         if tmp != []:
-            no_dataset.append(tmp)
+            # no_dataset.append(tmp)
+
+            # list1 = tmp[1:]
+            list1 = tmp[0:]
+            # print("list1",list1)
+            list2s = create_random_lists_float(range_start=0.1*-1, range_end=0.1, yousosu=len(target_kaisu_list), listsu=3)
+            for list2 in list2s:
+                # print("list2",list2)
+                result = [x + y for x, y in zip(list1, list2)]
+                # print("result",result)
+                # result.insert(0, dlist[dlist_retu])
+                # print("result_in",result)
+                no_dataset.append(result)
          
-    print("no_dataset_test",no_dataset)
+    # print("no_dataset_test",no_dataset)
     print("no_dataset_test_len",len(no_dataset))
 
     return no_dataset
 
 
 def create_random_lists(range_start, range_end, yousosu, listsu=6):
+
     random_lists = []
     for cnt in range(listsu):
         random.seed(cnt+1)
@@ -84,76 +93,80 @@ def create_random_lists(range_start, range_end, yousosu, listsu=6):
     return random_lists
 
 
+def create_random_lists_float(range_start, range_end, yousosu, listsu=6):
+    
+    random_lists = []
+    for cnt in range(listsu):
+        random.seed(cnt+1)
+        random_list = [round(random.uniform(range_start, range_end), 2) for _ in range(yousosu)]
+        random_lists.append(random_list)
+
+    print("random_lists",random_lists)
+    return random_lists
+
+
+def light_gbm(data, data2):
+
+    # 一列目のラベルを取得
+    labels = [row[0] for row in data]
+
+    # ラベルの個数をカウント
+    label_counts = Counter(labels)
+
+    # 一番個数が少ないラベルの数を取得
+    min_label_count = min(label_counts.values())
+
+    # 一番少ないラベルに合わせて他のラベルをフィルタリング
+    balanced_data = []
+    for label in label_counts.keys():
+        filtered_data = [row for row in data if row[0] == label][:min_label_count]
+        balanced_data.extend(filtered_data)
+
+    print("\nBalanced Data:")
+    for row in balanced_data:
+        print(row)
+
+    data = np.array(balanced_data)
+    X = data[:,1:]
+    print(X)
+    y = data[:,0]
+    print(y)
+
+    # データ分割
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # モデル
+    # max_depth=-1は無制限を意味する
+    model = lgb.LGBMClassifier(n_estimators=100, learning_rate=0.1, max_depth=-1, objective='multiclass')
+    model.fit(X_train, y_train)
+
+    # 評価
+    print("score", model.score(X_test, y_test))
+
+    # 推論
+    predictions = model.predict(data2)
+    print("Predictions:", predictions)
+
+    # 結果表示
+    # for true_label, prediction in zip(y_test, predictions):
+    #     print("True Label:", true_label)
+    #     print("Prediction:", prediction)
+    #     print()
+
+
 range_start = 1
 range_end = 24
 yousosu = 4
 target_kaisu_lists = create_random_lists(range_start, range_end, yousosu)
 
-dlists = dlists[1:500]
+st = 2
+dlists = dlists[st+1:500]
 data = no_dataset(dlists, target_kaisu_lists)
 
-dlists = dlists[0:500]
+dlists = dlists[st:500]
 data2 = no_dataset_test(dlists, target_kaisu_lists)
 
+light_gbm(data, data2)
 
-import numpy as np
-from collections import Counter
-
-# 一列目のラベルを取得
-labels = [row[0] for row in data]
-
-# ラベルの個数をカウント
-label_counts = Counter(labels)
-
-# 一番個数が少ないラベルの数を取得
-min_label_count = min(label_counts.values())
-
-# 一番少ないラベルに合わせて他のラベルをフィルタリング
-balanced_data = []
-for label in label_counts.keys():
-    filtered_data = [row for row in data if row[0] == label][:min_label_count]
-    balanced_data.extend(filtered_data)
-
-# 結果の表示
-# print("Original Data:")
-# for row in data:
-#     print(row)
-
-print("\nBalanced Data:")
-for row in balanced_data:
-    print(row)
-
-
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_iris
-# import pandas as pd
-import lightgbm as lgb
-
-data = np.array(balanced_data)
-X = data[:,1:]
-print(X)
-y = data[:,0]
-print(y)
-
-# トレーニングデータとテストデータに分割
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# モデル
-# max_depth=-1は無制限を意味する
-model = lgb.LGBMClassifier(n_estimators=100, learning_rate=0.1, max_depth=-1, objective='multiclass')
-model.fit(X_train, y_train)
-
-# 評価
-print("score", model.score(X_test, y_test))
-
-# 推論
-predictions = model.predict(data2)
-print("Predictions:", predictions)
-
-# 結果表示
-# for true_label, prediction in zip(y_test, predictions):
-#     print("True Label:", true_label)
-#     print("Prediction:", prediction)
-#     print()
 
 
