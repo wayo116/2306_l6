5fcb33a5522d01b523faa2fcf2ba62326dcadf51
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-07-13 15:49:55 +0900

    Update dataset.py

--- Utility/dataset.py
+++ Utility/dataset.py
@@ -98,14 +98,16 @@ def create_random_lists(range_start, range_end, yousosu, listsu=6):
 def create_random_lists_multi(range_start, range_end, yousosu, multisu, listsu=6):
 
     random_lists = []
-    for dummy_cnt in range(listsu):
+    for cnt1 in range(listsu):
+        cnt1 = cnt1 *10
         tmp = []
-        for cnt in range(multisu):
-            random.seed(cnt+1)
+        for cnt2 in range(multisu):
+            random.seed(cnt1+cnt2+1)
             random_list = random.sample(range(range_start, range_end + 1), yousosu)
             tmp.append(random_list)
             #print(tmp)
         random_lists.append(tmp)
+        
     # print("random_lists",random_lists)
     return random_lists
 
