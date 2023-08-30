3f7def9049d02578fd1004e14f4e913e26551cc8
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-08-31 08:08:02 +0900

    Add files via upload

--- main_v6_test.py
+++ main_v6_test.py
@@ -18,13 +18,13 @@ with open(csv_dir, "a", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(["kaisai",
         "saisinkekka_list",
-        "predictions",
+        "predictions_unique",
         "l1l2_len",
         "predictions_len",
         "percent" 
         ])
 
-for kaisai in range(1,6):
+for kaisai in range(1,20):
     # kaisai = -1
     if kaisai == -1:
         #本番
@@ -44,69 +44,114 @@ for kaisai in range(1,6):
 
     bunkatu=5
     predictions_all = []
+    predictions_delall = []
     lgbm_obj = LightgbmPack(makecsv=False)
+
 
     print('\n----vol 1----')
     params = {"train_params":{"range_start": 1,
-                        "range_end":11,
-                        "yousosu":10,
-                        "multisu":1,
-                        "randomkeisu":112,
-                        "nmasi":3},
+                        "range_end":200,
+                        "yousosu":100,
+                        "multisu":3,
+                        "randomkeisu":214,
+                        "nmasi":6},
         "test_params":{"range_start": 1,
-                        "range_end":11,
-                        "yousosu":10,
-                        "multisu":1,
-                        "randomkeisu":112,
+                        "range_end":200,
+                        "yousosu":100,
+                        "multisu":2,
+                        "randomkeisu":314,
                         "nmasi":1}}
 
     predictions = lgbm_obj.lightgbmpack(kaisai, saisinkekka_list, dlists, **params)
     predictions_all.extend(predictions)
 
     print('\n----vol 2----')
-    params = {"train_params":{"range_start": 11,
-                        "range_end":21,
-                        "yousosu":10,
-                        "multisu":1,
-                        "randomkeisu":112,
-                        "nmasi":3},
-        "test_params":{"range_start": 11,
-                        "range_end":21,
-                        "yousosu":10,
-                        "multisu":1,
-                        "randomkeisu":112,
+    params = {"train_params":{"range_start": 1,
+                        "range_end":200,
+                        "yousosu":101,
+                        "multisu":3,
+                        "randomkeisu":214,
+                        "nmasi":6},
+        "test_params":{"range_start": 1,
+                        "range_end":200,
+                        "yousosu":101,
+                        "multisu":2,
+                        "randomkeisu":314,
                         "nmasi":1}}
 
     predictions = lgbm_obj.lightgbmpack(kaisai, saisinkekka_list, dlists, **params)
     predictions_all.extend(predictions)
 
     print('\n----vol 3----')
-    params = {"train_params":{"range_start": 21,
-                        "range_end":31,
-                        "yousosu":10,
-                        "multisu":1,
-                        "randomkeisu":112,
-                        "nmasi":3},
-        "test_params":{"range_start": 21,
-                        "range_end":31,
-                        "yousosu":10,
-                        "multisu":1,
-                        "randomkeisu":112,
+    params = {"train_params":{"range_start": 1,
+                        "range_end":200,
+                        "yousosu":102,
+                        "multisu":3,
+                        "randomkeisu":214,
+                        "nmasi":6},
+        "test_params":{"range_start": 1,
+                        "range_end":200,
+                        "yousosu":102,
+                        "multisu":2,
+                        "randomkeisu":314,
                         "nmasi":1}}
 
     predictions = lgbm_obj.lightgbmpack(kaisai, saisinkekka_list, dlists, **params)
     predictions_all.extend(predictions)
 
 
+    print('\n----vol 1del----')
+    params = {"train_params":{"range_start": 1,
+                        "range_end":200,
+                        "yousosu":3,
+                        "multisu":1,
+                        "randomkeisu":214,
+                        "nmasi":1},
+        "test_params":{"range_start": 1,
+                        "range_end":200,
+                        "yousosu":3,
+                        "multisu":1,
+                        "randomkeisu":314,
+                        "nmasi":1}}
+
+    predictions = lgbm_obj.lightgbmpack(kaisai, saisinkekka_list, dlists, **params)
+    predictions_delall.extend(predictions)
+
+    print('\n----vol 2del----')
+    params = {"train_params":{"range_start": 1,
+                        "range_end":200,
+                        "yousosu":3,
+                        "multisu":1,
+                        "randomkeisu":314,
+                        "nmasi":1},
+        "test_params":{"range_start": 1,
+                        "range_end":200,
+                        "yousosu":3,
+                        "multisu":1,
+                        "randomkeisu":214,
+                        "nmasi":1}}
+
+
+    predictions = lgbm_obj.lightgbmpack(kaisai, saisinkekka_list, dlists, **params)
+    predictions_delall.extend(predictions)
+
     print("saisinkekka_list",saisinkekka_list)
     predictions_all = sorted(list(map(int, set(predictions_all))))
     print("predictions_all_set",predictions_all)
 
+    predictions_delall = sorted(list(map(int, set(predictions_delall))))
+    print("predictions_delall_set",predictions_delall)
+
+    predictions_unique = [item for item in predictions_all if item not in predictions_delall]
+
+    print(predictions_unique)
+
+
     l1 = saisinkekka_list
-    l2 = predictions_all
+    l2 = predictions_unique
     l1_l2_and = set(l1) & set(l2)
     l1l2_len = len(l1_l2_and)
-    predictions_len = len(predictions_all)
+    predictions_len = len(predictions_unique)
 
     percent = round(l1l2_len/predictions_len*100)
     print("percent",percent)
@@ -116,7 +161,7 @@ for kaisai in range(1,6):
         writer = csv.writer(file)
         writer.writerow([kaisai,
             saisinkekka_list,
-            predictions_all,
+            predictions_unique,
             l1l2_len,
             predictions_len,
             percent,
