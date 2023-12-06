6763b7af6c74d63aa440b310ee1861f996af6e0d
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-12-06 13:59:20 +0900

    Update per_hyouji.py

--- Utility/per_hyouji.py
+++ Utility/per_hyouji.py
@@ -1,1 +1,12 @@
-
+def per_hyouji(saisin,pred)
+    s_and_p = set(saisin) & set(pred)
+    s_and_p_len = len(s_and_p)
+    pred_len = len(pred)
+    
+    if s_and_p_len > 0 and pred_len > 0:
+        percent = round(s_and_p_len/pred_len*100)
+        print(f"{s_and_p_len}/{pred_len}")
+    else:
+        percent = 0
+    print("percent",percent)
+    print("\n")
