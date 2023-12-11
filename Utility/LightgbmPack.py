c7c8dbfab1bd3eaa6ff79e3fcd6c156eef8cbb9d
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-12-11 11:40:28 +0900

    Update LightgbmPack.py

--- Utility/LightgbmPack.py
+++ Utility/LightgbmPack.py
@@ -5,180 +5,6 @@ import time
 from datalists import dlists
 from Utility.dataset import no_dataset_trainval_multi, no_dataset_test_multi
 
-def light_gbm(train_data, test_data, **lgbm_params):
-    import lightgbm as lgb
-    
-    objective = lgbm_params["objective"]
-    num_class = lgbm_params["num_class"]
-    boosting_type = lgbm_params["boosting_type"]
-    metric = lgbm_params["metric"]
-    num_leaves = lgbm_params["num_leaves"]
-    learning_rate = lgbm_params["learning_rate"]
-    feature_fraction = lgbm_params["feature_fraction"]
-    max_depth = lgbm_params["max_depth"]
-    random_seed = lgbm_params["random_seed"]
-    force_row_wise = lgbm_params["force_row_wise"]
-    feature_pre_filter = lgbm_params["feature_pre_filter"]
-    lambda_l1 = lgbm_params["lambda_l1"]
-    lambda_l2 = lgbm_params["lambda_l2"]
-    bagging_fraction = lgbm_params["bagging_fraction"]
-    bagging_freq = lgbm_params["bagging_freq"]
-    min_child_samples = lgbm_params["min_child_samples"]
-    num_iterations = lgbm_params["num_iterations"]
-
-    # 一列目のラベルを取得
-    labels = [row[0] for row in train_data]
-
-    # ラベルの個数をカウント
-    label_counts = Counter(labels)
-
-    # 一番個数が少ないラベルの数を取得
-    min_label_count = min(label_counts.values())
-
-    # 一番少ないラベルに合わせて他のラベルをフィルタリング
-    balanced_data = []
-    for label in label_counts.keys():
-        filtered_data = [row for row in train_data if row[0] == label][:min_label_count]
-        balanced_data.extend(filtered_data)
-
-    # print("\nBalanced Data:")
-    # for row in balanced_data:
-    #     print(row)
-
-    data = np.array(balanced_data)
-    X = data[:,1:]
-    print(X)
-    y = data[:,0]-1
-    print(y)
-
-    # データ分割
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
-    dtrain = lgb.Dataset(X_train, y_train)
-    dvalid = lgb.Dataset(X_val, y_val)
-
-    params = {
-        'objective': objective,
-        'num_class': num_class,
-        'boosting_type': boosting_type,
-        'metric': metric,
-        'num_leaves': num_leaves,
-        'learning_rate': learning_rate,
-        'feature_fraction': feature_fraction,
-        'max_depth': max_depth,
-        'random_seed': random_seed,
-        'force_row_wise': force_row_wise,
-        'feature_pre_filter': feature_pre_filter,
-        'lambda_l1': lambda_l1,
-        'lambda_l2': lambda_l2,
-        'bagging_fraction': bagging_fraction,
-        'bagging_freq': bagging_freq,
-        'min_child_samples': min_child_samples,
-        'num_iterations': num_iterations,
-    }
-
-    # LightGBMモデルを訓練
-    model = lgb.train(params,dtrain, valid_sets=[dtrain, dvalid],)
-
-    # 評価
-    # score = model.score(dvalid)
-    # print("score", score)
-    preds_X_val = model.predict(X_val)
-    preds_X_val = np.argmax(preds_X_val, axis=1) + 1 # 予測結果のクラスの値を調整
-    accuracy = accuracy_score(y_val+1, preds_X_val)
-    print("accuracy",accuracy)
-
-    # 推論
-    # predictions = sorted(list(map(int, set(model.predict(test_data)+1))))
-    # print("Predictions:", predictions)
-    predictions = model.predict(test_data)
-    #print("predictions",predictions)
-    
-    predictions = np.argmax(predictions, axis=1) + 1 # 予測結果のクラスの値を調整
-    print("predictions",predictions)
-
-    return accuracy ,predictions
-
-
-def light_gbm_optuna(train_data, test_data, **lgbm_params):
-    import optuna.integration.lightgbm as lgb
-
-    objective = lgbm_params["objective"]
-    num_class = lgbm_params["num_class"]
-    boosting_type = lgbm_params["boosting_type"]
-    metric = lgbm_params["metric"]
-    num_leaves = lgbm_params["num_leaves"]
-    learning_rate = lgbm_params["learning_rate"]
-    feature_fraction = lgbm_params["feature_fraction"]
-    max_depth = lgbm_params["max_depth"]
-    random_seed = lgbm_params["random_seed"]
-    force_row_wise = lgbm_params["force_row_wise"]
-    feature_pre_filter = lgbm_params["feature_pre_filter"]
-    lambda_l1 = lgbm_params["lambda_l1"]
-    lambda_l2 = lgbm_params["lambda_l2"]
-    bagging_fraction = lgbm_params["bagging_fraction"]
-    bagging_freq = lgbm_params["bagging_freq"]
-    min_child_samples = lgbm_params["min_child_samples"]
-    num_iterations = lgbm_params["num_iterations"]
-
-    # 一列目のラベルを取得
-    labels = [row[0] for row in train_data]
-
-    # ラベルの個数をカウント
-    label_counts = Counter(labels)
-
-    # 一番個数が少ないラベルの数を取得
-    min_label_count = min(label_counts.values())
-
-    # 一番少ないラベルに合わせて他のラベルをフィルタリング
-    balanced_data = []
-    for label in label_counts.keys():
-        filtered_data = [row for row in train_data if row[0] == label][:min_label_count]
-        balanced_data.extend(filtered_data)
-
-    # print("\nBalanced Data:")
-    # for row in balanced_data:
-    #     print(row)
-
-    data = np.array(balanced_data)
-    X = data[:,1:]
-    print(X)
-    y = data[:,0]-1
-    print(y)
-
-    # データ分割
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
-    dtrain = lgb.Dataset(X_train, y_train)
-    dvalid = lgb.Dataset(X_val, y_val)
-
-    params = {
-        'objective': objective,
-        'num_class': num_class,
-        'boosting_type': boosting_type,
-        'metric': metric,
-        'num_leaves': num_leaves,
-        'learning_rate': learning_rate,
-        'feature_fraction': feature_fraction,
-        'max_depth': max_depth,
-        'random_seed': random_seed,
-        'force_row_wise': force_row_wise,
-        'feature_pre_filter': feature_pre_filter,
-        'lambda_l1': lambda_l1,
-        'lambda_l2': lambda_l2,
-        'bagging_fraction': bagging_fraction,
-        'bagging_freq': bagging_freq,
-        'min_child_samples': min_child_samples,
-        'num_iterations': num_iterations,
-    }
-
-    # LightGBMモデルを訓練
-    model = lgb.train(params,dtrain, valid_sets=[dtrain, dvalid],)
-    print("best params", model.params)
-    accuracy = 0
-    predictions = []
-    
-    return accuracy ,predictions
-   
-
 class LightgbmPack():
     def __init__(self):
         print("LightgbmPack")
@@ -189,7 +15,6 @@ class LightgbmPack():
 
         dataset_params = params["dataset_params"]
         lgbm_model = params["lgbm_model"]
-        print("lgbm_model",lgbm_model)
         lgbm_params = params["lgbm_params"]
 
         #学習検証用
@@ -216,10 +41,10 @@ class LightgbmPack():
         predictions = []
         print("model_type",lgbm_model["model_type"])
         if lgbm_model["model_type"] == "light_gbm":
-            score ,predictions = light_gbm(train_data, test_data, **lgbm_params)
+            score ,predictions = self.light_gbm(train_data, test_data, **lgbm_params)
             
         if lgbm_model["model_type"] == "light_gbm_optuna":
-            score ,predictions = light_gbm_optuna(train_data, test_data, **lgbm_params)
+            score ,predictions = self.light_gbm_optuna(train_data, test_data, **lgbm_params)
 
         # %計算
         l1 = saisinkekka_list
@@ -237,3 +62,176 @@ class LightgbmPack():
         print("処理時間",time.time() - start)
 
         return predictions
+
+    def light_gbm(self, train_data, test_data, **lgbm_params):
+        import lightgbm as lgb
+        
+        objective = lgbm_params["objective"]
+        num_class = lgbm_params["num_class"]
+        boosting_type = lgbm_params["boosting_type"]
+        metric = lgbm_params["metric"]
+        num_leaves = lgbm_params["num_leaves"]
+        learning_rate = lgbm_params["learning_rate"]
+        feature_fraction = lgbm_params["feature_fraction"]
+        max_depth = lgbm_params["max_depth"]
+        random_seed = lgbm_params["random_seed"]
+        force_row_wise = lgbm_params["force_row_wise"]
+        feature_pre_filter = lgbm_params["feature_pre_filter"]
+        lambda_l1 = lgbm_params["lambda_l1"]
+        lambda_l2 = lgbm_params["lambda_l2"]
+        bagging_fraction = lgbm_params["bagging_fraction"]
+        bagging_freq = lgbm_params["bagging_freq"]
+        min_child_samples = lgbm_params["min_child_samples"]
+        num_iterations = lgbm_params["num_iterations"]
+    
+        # 一列目のラベルを取得
+        labels = [row[0] for row in train_data]
+    
+        # ラベルの個数をカウント
+        label_counts = Counter(labels)
+    
+        # 一番個数が少ないラベルの数を取得
+        min_label_count = min(label_counts.values())
+    
+        # 一番少ないラベルに合わせて他のラベルをフィルタリング
+        balanced_data = []
+        for label in label_counts.keys():
+            filtered_data = [row for row in train_data if row[0] == label][:min_label_count]
+            balanced_data.extend(filtered_data)
+    
+        # print("\nBalanced Data:")
+        # for row in balanced_data:
+        #     print(row)
+    
+        data = np.array(balanced_data)
+        X = data[:,1:]
+        print(X)
+        y = data[:,0]-1
+        print(y)
+    
+        # データ分割
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+        dtrain = lgb.Dataset(X_train, y_train)
+        dvalid = lgb.Dataset(X_val, y_val)
+    
+        params = {
+            'objective': objective,
+            'num_class': num_class,
+            'boosting_type': boosting_type,
+            'metric': metric,
+            'num_leaves': num_leaves,
+            'learning_rate': learning_rate,
+            'feature_fraction': feature_fraction,
+            'max_depth': max_depth,
+            'random_seed': random_seed,
+            'force_row_wise': force_row_wise,
+            'feature_pre_filter': feature_pre_filter,
+            'lambda_l1': lambda_l1,
+            'lambda_l2': lambda_l2,
+            'bagging_fraction': bagging_fraction,
+            'bagging_freq': bagging_freq,
+            'min_child_samples': min_child_samples,
+            'num_iterations': num_iterations,
+        }
+    
+        # LightGBMモデルを訓練
+        model = lgb.train(params,dtrain, valid_sets=[dtrain, dvalid],)
+    
+        # 評価
+        # score = model.score(dvalid)
+        # print("score", score)
+        preds_X_val = model.predict(X_val)
+        preds_X_val = np.argmax(preds_X_val, axis=1) + 1 # 予測結果のクラスの値を調整
+        accuracy = accuracy_score(y_val+1, preds_X_val)
+        print("accuracy",accuracy)
+    
+        # 推論
+        # predictions = sorted(list(map(int, set(model.predict(test_data)+1))))
+        # print("Predictions:", predictions)
+        predictions = model.predict(test_data)
+        #print("predictions",predictions)
+        
+        predictions = np.argmax(predictions, axis=1) + 1 # 予測結果のクラスの値を調整
+        print("predictions",predictions)
+    
+        return accuracy ,predictions
+
+
+    def light_gbm_optuna(self, train_data, test_data, **lgbm_params):
+        import optuna.integration.lightgbm as lgb
+    
+        objective = lgbm_params["objective"]
+        num_class = lgbm_params["num_class"]
+        boosting_type = lgbm_params["boosting_type"]
+        metric = lgbm_params["metric"]
+        num_leaves = lgbm_params["num_leaves"]
+        learning_rate = lgbm_params["learning_rate"]
+        feature_fraction = lgbm_params["feature_fraction"]
+        max_depth = lgbm_params["max_depth"]
+        random_seed = lgbm_params["random_seed"]
+        force_row_wise = lgbm_params["force_row_wise"]
+        feature_pre_filter = lgbm_params["feature_pre_filter"]
+        lambda_l1 = lgbm_params["lambda_l1"]
+        lambda_l2 = lgbm_params["lambda_l2"]
+        bagging_fraction = lgbm_params["bagging_fraction"]
+        bagging_freq = lgbm_params["bagging_freq"]
+        min_child_samples = lgbm_params["min_child_samples"]
+        num_iterations = lgbm_params["num_iterations"]
+    
+        # 一列目のラベルを取得
+        labels = [row[0] for row in train_data]
+    
+        # ラベルの個数をカウント
+        label_counts = Counter(labels)
+    
+        # 一番個数が少ないラベルの数を取得
+        min_label_count = min(label_counts.values())
+    
+        # 一番少ないラベルに合わせて他のラベルをフィルタリング
+        balanced_data = []
+        for label in label_counts.keys():
+            filtered_data = [row for row in train_data if row[0] == label][:min_label_count]
+            balanced_data.extend(filtered_data)
+    
+        # print("\nBalanced Data:")
+        # for row in balanced_data:
+        #     print(row)
+    
+        data = np.array(balanced_data)
+        X = data[:,1:]
+        print(X)
+        y = data[:,0]-1
+        print(y)
+    
+        # データ分割
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+        dtrain = lgb.Dataset(X_train, y_train)
+        dvalid = lgb.Dataset(X_val, y_val)
+    
+        params = {
+            'objective': objective,
+            'num_class': num_class,
+            'boosting_type': boosting_type,
+            'metric': metric,
+            'num_leaves': num_leaves,
+            'learning_rate': learning_rate,
+            'feature_fraction': feature_fraction,
+            'max_depth': max_depth,
+            'random_seed': random_seed,
+            'force_row_wise': force_row_wise,
+            'feature_pre_filter': feature_pre_filter,
+            'lambda_l1': lambda_l1,
+            'lambda_l2': lambda_l2,
+            'bagging_fraction': bagging_fraction,
+            'bagging_freq': bagging_freq,
+            'min_child_samples': min_child_samples,
+            'num_iterations': num_iterations,
+        }
+    
+        # LightGBMモデルを訓練
+        model = lgb.train(params,dtrain, valid_sets=[dtrain, dvalid],)
+        print("best params", model.params)
+        accuracy = 0
+        predictions = []
+        
+        return accuracy ,predictions
