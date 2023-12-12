ebf195a5e28ff2de893edbd464c20f5d36fc2366
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-12-12 17:04:13 +0900

    Update dataset.py

--- Utility/dataset.py
+++ Utility/dataset.py
@@ -417,7 +417,7 @@ def remove_outliers(train_data, threshold):
     # ラベル毎に、Zスコアが指定された閾値以上の列を含む外れ値を削除する
     for prelabel in range(43):
         prelabel_train_data = train_data[train_data[:, 0] == prelabel + 1, :]
-        nolabel_train_data = prelabel_train_data[1:,:]
+        nolabel_train_data = prelabel_train_data[:,1:]
         print("nolabel_train_data",nolabel_train_data)
         
         z_scores = np.abs((nolabel_train_data - np.mean(nolabel_train_data, axis=0)) / np.std(nolabel_train_data, axis=0))
