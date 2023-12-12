afeeb55a325bb8b78e1575dfba7d8a64fee27915
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-12-12 18:03:59 +0900

    Update dataset.py

--- Utility/dataset.py
+++ Utility/dataset.py
@@ -425,8 +425,8 @@ def remove_outliers(train_data, z_thresh):
         print("z_scores",z_scores)
         outliers = np.any(z_scores > z_thresh, axis=1)
         print("outliers",outliers)
-        for iii in prelabel_train_data[~outliers]:
-            re_train_data.append(iii.tolist())
+        for one_array_data in prelabel_train_data[~outliers]:
+            re_train_data.append(one_array_data.tolist())
     print("re_train_data",re_train_data)
     return re_train_data
     
