7b3df8379c8a8eda735aff55769e60c97d4a8411
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-12-15 10:12:08 +0900

    Update dataset.py

--- Utility/dataset.py
+++ Utility/dataset.py
@@ -404,8 +404,8 @@ def create_random_lists_float(range_start, range_end, yousosu, listsu=6):
     random_lists = []
     for cnt in range(listsu):
         random.seed(cnt+1)
-        #random_list = [round(random.uniform(range_start, range_end), 2) for _ in range(yousosu)]
-        random_list = [round(random.uniform(range_start, range_end),2)]*yousosu
+        random_list = [round(random.uniform(range_start, range_end), 2) for _ in range(yousosu)]
+        #random_list = [round(random.uniform(range_start, range_end),2)]*yousosu
         random_lists.append(random_list)
 
     # print("random_lists",random_lists)
