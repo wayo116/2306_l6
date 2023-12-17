24ffe94cf4f0a01cd7d8c6697feb411def863a8b
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-12-17 11:49:44 +0900

    Update dataset.py

--- Utility/dataset.py
+++ Utility/dataset.py
@@ -52,9 +52,9 @@ def no_dataset_trainval_multi(dlists, **dataset_params,):
             # 最大
             max_n = np.max(dlists[kaisu+shokichi:kaisu+shokichi+bunseki_hani, dlist_retu])
             # 範囲
-            #range_value = max_n - min_n
+            range_value = max_n - min_n
             # 平均
-            mean_n = np.mean(dlists[kaisu+shokichi:kaisu+shokichi+bunseki_hani, dlist_retu])
+            #mean_n = np.mean(dlists[kaisu+shokichi:kaisu+shokichi+bunseki_hani, dlist_retu])
             # 合計
             sum_n = np.sum(dlists[kaisu+shokichi:kaisu+shokichi+bunseki_hani, dlist_retu])
             # 中央
