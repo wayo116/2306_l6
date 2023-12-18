d7bbd69982632b2cca57c99824e23d992e40e584
wayo116 <101450166+wayo116@users.noreply.github.com>
2023-12-18 14:09:08 +0900

    Update dataset.py

--- Utility/dataset.py
+++ Utility/dataset.py
@@ -441,7 +441,7 @@ def no_dataset_test_multi(dlists, **dataset_params):
             for list1_3_nmasi in np.concatenate((np.array(list1_nmasi),np.array(list3_nmasi)), axis = 1):
                 no_dataset.append(list1_3_nmasi)
             
-    # print("no_dataset",no_dataset)
+    print("no_dataset",no_dataset)
     print("no_dataset_rows",len(no_dataset))
     print("no_dataset_columns",len(no_dataset[0]))
 
